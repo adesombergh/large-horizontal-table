d07ad32c63b02ef6eecdd054b857167d74eea52a
Aldebaran Desombergh <aldebaran.desombergh@idweaver.com>
2021-01-13 10:09:38 +0100

    Place output on Pages dir & fix build script

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
+console.log('PAPAPAPAPPAPAPAPA', process.env.NODE_ENV)
 module.exports = {
+  outputDir: 'pages',
   publicPath: process.env.NODE_ENV === 'production'
     ? '/large-horizontal-table/'
     : '/'
